0daeb926b81ded5b6bb5ead0b2716ea6752672f8
Adam Figgins <adam.figgins@nweh.co.uk>
2022-09-23 14:32:56 +0100

    Pwsh - Adding more config and autocomplete plugin

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -4,9 +4,13 @@
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Install-Module -Name Terminal-Icons -Repository PSGallery
+Install-Module -Name PSReadLine -AllowPrerelease -Scope AllUsers -Force -SkipPublisherCheck
 if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
   New-Item -ItemType File -Path $PROFILE.AllUsersAllHosts -Force
 }
-Add-Content -Value "oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression" -Path $PROFILE.AllUsersAllHosts
+Add-Content -Value "oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH\easy-term.omp.json | Invoke-Expression" -Path $PROFILE.AllUsersAllHosts
 Add-Content -Value "Import-Module -Name Terminal-Icons" -Path $PROFILE.AllUsersAllHosts
+Add-Content -Value "Set-PSReadLineOption -PredictionSource History" -Path $PROFILE.AllUsersAllHosts
+Add-Content -Value "Set-PSReadLineOption -PredictionViewStyle ListView" -Path $PROFILE.AllUsersAllHosts
+Add-Content -Value "Set-PSReadLineOption -EditMode Windows" -Path $PROFILE.AllUsersAllHosts
 refreshenv
