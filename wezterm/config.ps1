a0789ed612096b2cc7b02c3cfc3e77db56a0d0bb
issafalcon <atfiggins@gmail.com>
2023-04-07 21:15:12 +0100

    feat(wezterm): Adds symlink for custom local projects

--- wezterm/config.ps1
+++ wezterm/config.ps1
@@ -3,8 +3,17 @@ param (
 )
 
 $weztermConfigDir = "${installationdrive}:/repos/wezterm-config"
+$weztermCustomProjectDir =  "~/.config/wezterm-projects"
 
 mkdir ${installationdrive}:/repos/
 git clone https://github.com/Issafalcon/wezterm-config.git $weztermConfigDir
+
+if (Test-Path $weztermCustomProjectDir)
+{
+  "WezTerm projects folder exists. Skipping creation"
+} else
+{
+  New-Item -ItemType Directory -Path $weztermCustomProjectDir
+}
 
 New-Item -ItemType SymbolicLink -Path "~/.config/wezterm" -Target $weztermConfigDir
