445b1d0f5c7db433c43a256339d8d1b216535db3
Adam Figgins <adam.figgins@nweh.co.uk>
2023-04-14 11:35:36 +0100

    fix(python): Fixes venv install of python modules
    - Before they used the global python exe to install global pip packages
    - Using py command instead use the local venv exe for python

--- python3/install.ps1
+++ python3/install.ps1
@@ -9,18 +9,25 @@ scoop install python311
 python3 -m pip install --upgrade pip
 
 $currentDIr = Get-Location
+
+if ($installationdrive -eq "default")
+{
+  $installationdrive = "C"
+}
+
 Set-Location "${installationdrive}:\Applications\Scoop\apps\neovim"
 mkdir python3
 Set-Location python3
 mkdir Envs
 Set-Location Envs
 
-if ($createneovimenv -eq $true) {
+if ($createneovimenv -eq $true)
+{
   python3 -m venv neovim # Create the virtual env for python3
   .\neovim\Scripts\activate
-  python3 -m pip install pynvim
-  python3 -m pip install neovim
-  python3 -m pip install neovim-remote
+  py -m pip install pynvim
+  py -m pip install neovim
+  py -m pip install neovim-remote
   deactivate
 
   mkdir "~/AppData/Local/python3/Envs"
