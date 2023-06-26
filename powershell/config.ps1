b03d701b6e7043b9500ee4e33c79015b1975a40e
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2023-06-26 14:28:37 +0100

    fix(powershell): Fixes spaces in oh-my-posh profile
    - Spaces in omp path were cuasing issues when loading omp

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -8,7 +8,9 @@ Install-Module -Name PSReadLine -Scope AllUsers -Force -SkipPublisherCheck
 if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
   New-Item -ItemType File -Path $PROFILE.AllUsersAllHosts -Force
 }
-Add-Content -Value "oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH\easy-term.omp.json | Invoke-Expression" -Path $PROFILE.AllUsersAllHosts
+
+Clear-Content $PROFILE.AllUsersAllHosts -Force
+Add-Content -Value "oh-my-posh --init --shell pwsh --config `$env:POSH_THEMES_PATH`\easy-term.omp.json | Invoke-Expression" -Path $PROFILE.AllUsersAllHosts
 Add-Content -Value "Import-Module -Name Terminal-Icons" -Path $PROFILE.AllUsersAllHosts
 Add-Content -Value "Set-PSReadLineOption -PredictionSource History" -Path $PROFILE.AllUsersAllHosts
 Add-Content -Value "Set-PSReadLineOption -PredictionViewStyle ListView" -Path $PROFILE.AllUsersAllHosts
