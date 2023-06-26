af6ba84e93dfc4dd0b0bf480a0de6c7611ce0e90
Adam Figgins <atfiggins@gmail.com>
2023-06-26 11:23:51 +0100

    Update install.ps1
    Removed default to C drive logic in scoop install script

--- scoop/install.ps1
+++ scoop/install.ps1
@@ -1,13 +1,9 @@
 param (
-  [Parameter(Mandatory)] $installationdrive = "default"
+  [Parameter] $installationdrive = "C"
 )
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod get.scoop.sh -outfile 'install.ps1'
-
-if ($installationdrive -eq "default") {
-  $installationdrive = "C"
-}
 
 & .\install.ps1 -ScoopDir "${installationdrive}:\Applications\Scoop" -ScoopGlobalDir "${installationdrive}:\GlobalScoopApps" -NoProxy
 
