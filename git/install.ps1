0f39c93657d0f1bc22995231c05afe3e43219c63
Adam Figgins <atfiggins@gmail.com>
2024-10-14 14:16:18 +0100

    chore(git): Updating installer

--- git/install.ps1
+++ git/install.ps1
@@ -2,7 +2,7 @@ param (
   $installationdrive = "C"
 )
 
-# Git
-winget install --id Git.Git -e --source winget --location "${installationdrive}:\Program Files\Git"
-[System.Environment]::SetEnvironmentVariable('Path', $env:Path + ";${installationdrive}:\Program Files\Git\cmd", "Machine")
+# Git - Install isn't needed as git needs to be installed manually first anyway as a pre-requisite
+# winget install --id Git.Git -e --source winget --location "${installationdrive}:\Program Files\Git"
+# [System.Environment]::SetEnvironmentVariable('Path', $env:Path + ";${installationdrive}:\Program Files\Git\cmd", "Machine")
 scoop install delta
